5b745137aaf10d4d8d763b48c1672ffcee302f49
ZekkLiu <yoyo6112763@gmail.com>
2023-01-03 23:39:09 +0800

    Update math1-1.c

--- ITSA/math1-1.c
+++ ITSA/math1-1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    
+    int  top, bottom, height;
+    float area;
+    scanf("%d %d %d", &top, &bottom, &height);
+    area = ((top + bottom) * height) / 2;
+    printf("Trapezoid area:%.1f\n", area);
+    return 0;
 }
