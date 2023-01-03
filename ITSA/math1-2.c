ed4503ca17c022309f35626098b21c954199ffe7
ZekkLiu <yoyo6112763@gmail.com>
2023-01-03 23:47:11 +0800

    Update math1-2.c

--- ITSA/math1-2.c
+++ ITSA/math1-2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    
+    int bottom, height;
+    float area;
+    scanf("%d %d", &bottom, &height);
+    area = bottom * height / 2.0;
+    printf("%.1f\n", area);
+    return 0;
 }
